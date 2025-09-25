bddb2b4b1a10fde1d719a62b1b4ec028e444e8c2
rj_m <rjmarte@sbcglobal.net>
2025-09-25 14:24:30 -0700

    branch for if/else

--- js/script.js
+++ js/script.js
@@ -1,8 +1,14 @@
 var button = document.querySelector(".show-cat");
 var cat = document.querySelector(".cat");
 
-// console.log("button");
 button.addEventListener("click", function () {
-  //   console.log("it's working!");
-  cat.classList.add("show");
+  if (cat.classList.contains("show")) {
+    cat.classList.remove("show");
+    button.innerText = "Wait, come back!";
+    button.classList.add("disappear");
+  } else {
+    cat.classList.add("show");
+    button.innerText = "Shoo, cat!";
+    button.classList.remove("disappear");
+  }
 });
